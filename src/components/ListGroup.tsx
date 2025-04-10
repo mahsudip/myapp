d9d8f615b166168083063044018b9c4471d7324a
MahSudip <sudip@sudip-maharjan-Mac-mini.local>
2025-04-10 11:56:33 +0545

    active list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,19 @@
+import { useState } from "react";
+
 function ListGroup() {
   const items = ["A", "B", "C"];
+  const[selected, SelectedFunction] = useState(-1);
 
   return (
     <>
       <h1>List</h1>
       {items.length == 0 && <p>There are no Lists.</p>}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
             key={item}
-            className="list-group-item"
-            onClick={() => console.log("Clicked")}
+            className={selected == index ? "list-group-item active" : "list-group-item"}
+            onClick={() => SelectedFunction(index)}
           >
             {item}
           </li>
